3d87ea88d2b70c448e37f34cd5e8342fc9e0b0d4
Navtoj Chahal <github@navtoj.com>
2024-02-22 23:53:25 -0800

    🔧 fix: format shell commands for better usability

--- zod_to_json_schema/test.ts
+++ zod_to_json_schema/test.ts
@@ -1,8 +1,11 @@
 // deno run --allow-all test.ts
 import { z } from 'https://deno.land/x/zod@v3.20.2/index.ts';
 
-// import { zodToJsonSchema } from 'npm:zod-to-json-schema@latest';
+// this does NOT work
 import { zodToJsonSchema } from 'https://deno.land/x/zod_to_json_schema@3.20.3/zodToJsonSchema.ts';
+
+// this works
+//import { zodToJsonSchema } from 'npm:zod-to-json-schema@latest';
 
 const schema = z.string();
 
