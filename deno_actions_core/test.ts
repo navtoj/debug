4613b8298020170938524e3546d48b4c4daa4a82
Navtoj Chahal <github@navtoj.com>
2024-03-06 00:06:17 -0800

    Delete deno_actions_core directory
    Signed-off-by: Navtoj Chahal <github@navtoj.com>

--- deno_actions_core/test.ts
+++
@@ -1,9 +0,0 @@
-// deno run --allow-all test.ts
-
-// this does NOT work
-import { info } from 'https://deno.land/x/deno_actions_core@0.3.0/mod.ts';
-
-// this works
-//import { info } from 'npm:@actions/core';
-
-info('Hello World!');
